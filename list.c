643b5738f90ed39228c09f7c82807324575cb921
Borja Tarraso <borja.tarraso@member.fsf.org>
2012-10-28 12:24:33 +0200

    Created internal structure for linked list

---
+++ list.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _node{
+  int data;
+  struct _node *next;
+} nodeType;
+
+typedef nodeType *pNode;
+typedef nodeType *pList;
+
+int
+main( int argc, char *argv )
+{
+  
+  return 0;
+}
