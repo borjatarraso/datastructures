281d29495bc5bd92737f3c2ce2c5d4f50a841e5b
Borja Tarraso <borja.tarraso@member.fsf.org>
2012-11-08 21:45:18 +0200

    Implemented display_linked_list() and is_linked_list() for linked lists

--- list.c
+++ list.c
@@ -9,6 +9,28 @@ typedef struct _node{
 typedef nodeType *pNode;
 typedef nodeType *pList;
 
+int is_linked_list_empty( pList l )
+{
+  return ( l == NULL );
+}
+
+void display_linked_list( pList l )
+{
+  pNode node;
+
+  node = l;
+
+  if( !l )
+    printf("Empty linked list\n");
+  else{
+    while( node ){
+      printf("%d -> ", node->data);
+      node = node->next;
+    }
+    printf("\n");
+  }
+}
+
 int
 main( int argc, char *argv )
 {
