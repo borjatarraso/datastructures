5d0b7e275bb02c98cb9c3817ec8ab2c4a903a894
Borja Tarraso <borja.tarraso@member.fsf.org>
2024-06-03 19:48:20 +0300

    dlist: added double linked list implementation

---
+++ dlist/dlist.c
@@ -0,0 +1,120 @@
+/*
+ * This file is a part of DS Project
+ *
+ * dlist.c - doubly linked list implementation
+ * Copyright (C) 2024  Borja Tarraso <borja.tarraso@member.fsf.org>
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "dlist.h"
+
+void dlist_init(DoubleList *list, void (*destroy)(void *data)){
+    list->size = 0;
+    list->destroy = destroy;
+    list->head = NULL;
+    list->tail = NULL;
+}
+
+void dlist_destroy(DoubleList *list){
+    void *data;
+
+    while(dlist_size(list) > 0){
+        if(dlist_remove(list, dlist_tail(list), (void **)&data) == 0 && list->destroy != NULL){
+            list->destroy(data);
+        }
+    }
+
+    memset(list, 0, sizeof DoubleList);
+}
+
+int dlist_ins_next(DoubleList *list, DoubleListElement *element, const void *data){
+    DoubleListElement *new_element;
+
+    if(element == NULL && dlist_size(list) != 0)
+        return -1;
+
+    if((new_element = (DoubleListElement *)malloc(sizeof DoubleListElement)) == NULL)
+        return -1;
+
+    new_element->data = (void *)data;
+
+    if(dlist_size(list) == 0){
+        list->head = new_element;
+        list->head->prev = NULL;
+        list->head->next = NULL;
+        list->tail = new_element;
+    }else{
+        new_element->next = element->next;
+        new_element->prev = element;
+
+        if(element->next == NULL)
+            list->tail = new_element;
+        else
+            element->next->prev = new_element;
+
+        element->next = new_element;
+    }
+
+    list->size++;
+
+    return 0;
+}
+
+int dlist_ins_prev(DoubleList *list, DoubleListElement *element, const void *data){
+    if(element == NULL && dlist_size(list) != 0)
+        return -1;
+
+    if((new_element = (DoubleListElement *)malloc(sizeof DoubleListElement)) == NULL)
+        return -1;
+
+    new_element->data = (void *)data;
+
+    if(dlist_size(list) == 0){
+        list->head = new_element;
+        list->head->prev = NULL;
+        list->head->next = NULL;
+        list->tail = new_element;
+    }else{
+        new_element->next = element;
+        new_element->prev = element->prev;
+
+        if(element->prev == NULL)
+            list->head = new_element;
+        else
+            element->prev->next = new_element;
+
+        element->prev = new_element;
+    }
+
+    list->size++;
+    return 0;
+}
+
+int dlist_remove(DoubleList *list, DoubleListElement *element, void **data){
+    if(element == NULL || dlist_size(list) == 0)
+        return -1;
+
+    *data = element->data;
+
+    if(element == list->head){
+        list->head = element->next;
+
+        if(list->head == NULL)
+            list->tail = NULL;
+        else
+            element->next->prev = NULL;
+    }else{
+        element->prev->next == element->next;
+
+        if(element->next == NULL)
+            list->tail = element->prev;
+        else
+            element->next->prev = element->prev;
+    }
+
+    free(element);
+    list->size--;
+    return 0;
+}
---
+++ dlist/dlist.h
@@ -0,0 +1,44 @@
+/*
+ * This file is a part of DS Project
+ *
+ * dlist.h - doubly linked list header
+ * Copyright (C) 2024  Borja Tarraso <borja.tarraso@member.fsf.org>
+ */
+
+#ifndef DLIST_H
+#define DLIST_H
+
+#include <stdlib.h>
+
+typedef struct DoubleListElement_ {
+    void *data;
+    struct DoubleListElement_ *prev;
+    struct DobuleListElement_ *next;
+} DoubleListElement;
+
+typedef struct DoubleList_ {
+    int size;
+
+    int (*match)(const void *key1, const void *key2);
+    void (*destroy)(void *data);
+
+    DoubleListElement *head;
+    DoubleListElement *tail;
+} DoubleList;
+
+void dlist_init(DoubleList *list, void (*destroy)(void *data));
+void dlist_destroy(DoubleList *list);
+int dlist_ins_next(DoubleList *list, DoubleListElement *element, const void *data);
+int dlist_ins_prev(DoubleList *list, DoubleListElement *element, const void *data);
+int dlist_remove(DoubleList *list, DoubleListElement *element, void **data);
+
+#define dlist_size(list) ((list)->size)
+#define dlist_head(list) ((list)->head)
+#define dlist_tail(list) ((list)->tail)
+#define dlist_is_head(element) ((element)->prev == NULL ? 1 : 0)
+#define dlist_is_tail(element) ((element)->next == NULL ? 1 : 0)
+#define dlist_data(element) ((element)->data)
+#define dlist_next(element) ((element)->next)
+#define dlist_prev(element) ((element)->prev)
+
+#endif
